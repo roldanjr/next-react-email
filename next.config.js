53fa95ce874d683a5d55b17d685aaea72c305316
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2023-10-13 19:42:49 +0800

    Improve Developer Experience

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination:
+          process.env.NODE_ENV === "production"
+            ? "https://roldanjr.github.io/next-react-email/"
+            : "http://localhost:4000/",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
